9e38a311af7a63e187ae45632cb9a8d21339e727
nrosanta <nrosanta@gmail.com>
2022-01-11 14:40:22 -0500

    Update aws.config.ts

--- src/environments/aws.config.ts
+++ src/environments/aws.config.ts
@@ -6,11 +6,11 @@ export class AwsConfig {
         
     private _authConfig = {
         region: 'us-east-1',
-        userPoolId: 'us-east-1_4dKQSHHLN',
-        identityPoolId: 'us-east-1:516e0650-f02e-4be0-9eb2-a9e30be377dd',
+        userPoolId: 'us-east-1-test',
+        identityPoolId: 'test',
         userPoolWebClientId: '3ksst9434hpe0g35lq4dbqobp0',
         oauth: {
-            domain: 'cci-acoe-tofu-dev.auth.us-east-1.amazoncognito.com',
+            domain: 'test.com',
             scope: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
             redirectSignIn: 'https://localhost:4200/',
             redirectSignOut: 'https://localhost:4200',
